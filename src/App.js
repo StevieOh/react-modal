732038e5972bef7b0efa2c0a72fe14810d14079c
Stephanie Obeytek <sobeytek@gmail.com>
2018-11-07 20:21:32 -0600

    functioning Alert window

--- src/App.js
+++ src/App.js
@@ -3,64 +3,59 @@ import './App.css';
 import Modal from 'react-modal';
 
 class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isShowingModal: false,
-//       warningTime: 1000 * 10,
-//       finWarnTime: 1000 * 15,
-//       };
-//     }
-//     handleClose = () => 
-//       this.setState({isShowingModal: false})
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowingModal: false,
+      warningTime: 1000 * 10,
+      finWarnTime: 1000 * 15,
+      };
+    }
 
-//     componentDidMount() {
-//       this.events = [
-//         'load',
-//         'mousemove',
-//         'mousedown',
-//         'click',
-//         'scroll',
-//         'keypress'
-//       ];
-//       for (var i in this.events) {
-//         window.addEventListener(this.events[i], this.resetTimeout);
-//       }
-//       this.setTimeout();
-//     }
-//     clearTimeoutFunc = () => {
-//       if (this.warnTimeout) clearTimeout(this.warnTimeout);
+    componentDidMount() {
+      this.events = [
+        'load',
+        'mousemove',
+        'mousedown',
+        'click',
+        'scroll',
+        'keypress'
+      ];
+      for (var i in this.events) {
+        window.addEventListener(this.events[i], this.resetTimeout);
+      }
+      this.setTimeout();
+    }
+    clearTimeoutFunc = () => {
+      if (this.warnTimeout) clearTimeout(this.warnTimeout);
 
-//       if (this.finWarnTimeout) clearTimeout(this.finWarnTimeout);
-//     };
+      if (this.finWarnTimeout) clearTimeout(this.finWarnTimeout);
+    };
 
-//     setTimeout = () => {
-//       this.warnTimeout = setTimeout(this.warn, this.state.warningTime);
-//       this.finWarnTimeout = setTimeout(this.finWarn, this.state.finWarnTime);
-//     };
+    setTimeout = () => {
+      this.warnTimeout = setTimeout(this.warn, this.state.warningTime);
+      this.finWarnTimeout = setTimeout(this.finWarn, this.state.finWarnTime);
+    };
 
-//     resetTimeout = () => {
-//       this.clearTimeoutFunc();
-//       this.setTimeout();
-//     };
+    resetTimeout = () => {
+      this.clearTimeoutFunc();
+      this.setTimeout();
+    };
 
-//     warn = () => {
-//       // window.alert("Hey! Are you still there?")
-//     };
+    warn = () => {
+      window.alert("Hey! Are you still there?")
+    };
 
-//     finWarn = () => {
-//       window.alert("No, really... are you still there?")
-//     };
+    finWarn = () => {
+      window.alert("No, really... are you still there?")
+    };
 
     render() {
 
       return (
         <div>
         </div>
-      );
-//       // {this.state.isShowingModal &&
-//       //   <ModalComponentHere onClose={this.handleClose}/>
-//       // }
+      )
     }
   }
 
